be02ab488fe4bce3c7295d299ce0748037ae7ada
simonedesordi <simone.desordi@gmail.com>
2015-08-22 11:02:27 +0200

    updated .gitignore

--- src/app-old.js
+++ src/app-old.js
@@ -14,7 +14,7 @@ var auth = express.basicAuth(function(user, pass, callback) {
 
 app.configure(function() {
     app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
+    app.set('views', __dirname + '/../views');
     app.set('view engine', 'ejs');
     app.use(express.favicon());
     app.use(express.logger('dev'));
